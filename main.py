ad582a3124339090e0027c0e9603b1b0913f559e
milankumawat1 <milankumawat01@gmail.com>
2023-08-26 07:41:18 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template,request
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk 
+nltk.download('vader_lexicon')
+
+app=Flask(__name__)
+
+@app.route('/',methods=["GET","POST"])
+def main():
+    if request.method=="POST":
+        input=request.form.get("input")
+        sid=SentimentIntensityAnalyzer()
+        score=sid.polarity_scores(input)
+        if score["neg"]!=0:
+            return render_template('home.html', message="Negative😢😢")
+        else:
+            return render_template('home.html', message="Positive😊😊")
+    return render_template('home.html')
+
